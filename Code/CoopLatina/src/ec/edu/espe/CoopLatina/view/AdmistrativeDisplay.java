78aa60723cb0e8fa84ed686dcdc17c14c4c0acba
Molina Gustavo <stevemolin2001@hotmail.com>
2022-07-16 22:24:27 -0500

    v1.0.1

--- Code/CoopLatina/src/ec/edu/espe/CoopLatina/view/AdmistrativeDisplay.java
+++ Code/CoopLatina/src/ec/edu/espe/CoopLatina/view/AdmistrativeDisplay.java
@@ -25,13 +25,9 @@ import java.util.Scanner;
  * @author Loor Cesar, Molina Gustavo,DDCO-ESPE,GADC.MSI
  */
 public class AdmistrativeDisplay {
-<<<<<<< HEAD
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         System.out.println("==========Welcome to Coop-Latina==========");
-=======
-    public static void main(String[] args) throws FileNotFoundException, IOException {
->>>>>>> e128399f8cabff6f91b3d6a130613249403e6329
-        
         System.out.println("\t=========================");
         System.out.println("\t==WELCOME TO COOPLATINA==");
         System.out.println("\t=========================");
